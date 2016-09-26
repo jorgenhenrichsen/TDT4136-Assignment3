ae56ccb14ce3e5908cc4ac1b439313fd4edb226c
jorgenhenrichsen <jh.henrichs@gmail.com>
2016-09-26 14:34:56 +0200

    Initial commit

---
+++ Assignment3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Jørgen Henrichsen on 26/09/16.
+//  Copyright © 2016 Jørgen Henrichsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
